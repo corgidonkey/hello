04d5df4397d59536a660e356c59cd6b518c0dc49
shijiabing <shijiabing@mobike.com>
2018-04-28 00:03:25 +0800

    update

--- app/src/main/java/com/example/shijiabing/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/shijiabing/myapplication/MainActivity.kt
@@ -7,6 +7,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
     }
 }
